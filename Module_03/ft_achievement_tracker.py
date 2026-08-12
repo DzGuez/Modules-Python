91f11415b01eaf134ec4b997636a5a48818c7e14
Cristian David Blanco Rodriguez <crblanco@car5s6.42barcelona.com>
2026-08-12 12:18:47 +0200

    Module 03 ejercicio 04 12 August

--- Module_03/ft_achievement_tracker.py
+++ Module_03/ft_achievement_tracker.py
@@ -35,11 +35,45 @@ def gen_player_achievements() -> set[str]:
 
 
 def main() -> None:
-    """Realiza la ejecucion del sistema de coordenadas"""
+    """Realiza la ejecucion del sistema de los logros para jugadores"""
     """Genera logros para 4 jugadores y muestra estadisticas de esos logros"""
     print("=== Achievement Tracker System ===")
 
     jugadores = ["Alice", "Bob", "Charlie", "Dylan"]
     logros_jugadores: dict[str, set[str]] = {}
 
-    
+    for jugador in jugadores:
+        logros_jugadores[jugador] = gen_player_achievements()
+        print(f"\nPlayer {jugador}: {logros_jugadores[jugador]}")
+
+
+    todos_los_logros: set[str] = set()
+    for logros in logros_jugadores.values():
+        todos_los_logros = todos_los_logros.union(logros)
+    print(f"\nAll distinct achievements: {todos_los_logros}")
+
+
+    sets_jugadores = list(logros_jugadores.values())
+    comunes = sets_jugadores[0]
+    for logros in sets_jugadores[1:]:
+        comunes = comunes.intersection(logros)
+    print(f"\nCommon achievements: {comunes}\n")
+
+
+    for jugador in jugadores:
+        otros: set[str] = set()
+        for otro_jugador, otros_logros in logros_jugadores.items():
+            if otro_jugador != jugador:
+                otros = otros.union(otros_logros)
+        solo_este = logros_jugadores[jugador].difference(otros)
+        print(f"Only {jugador} has: {solo_este}")
+
+
+    for jugador in jugadores:
+        faltantes = todos_los_logros.difference(logros_jugadores[jugador])
+        print(f"\n{jugador} is missing: {faltantes}")
+
+
+if __name__ == "__main__":
+    main()
+
