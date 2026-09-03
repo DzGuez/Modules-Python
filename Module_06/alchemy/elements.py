0bb5863e53cfd4cf14e57f328a4d53fd87b6541b
Cristian David Blanco Rodriguez <crblanco@cbr4s5.42barcelona.com>
2026-09-03 18:34:25 +0200

    Module 06 Directorio 3 sept

---
+++ Module_06/alchemy/elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+def create_earth() -> None:
+
+
+def create_air() -> None:
---
+++ Module_06/elements.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+
+
+def create_fire() -> None:
+
+
+def create_water() -> None:
